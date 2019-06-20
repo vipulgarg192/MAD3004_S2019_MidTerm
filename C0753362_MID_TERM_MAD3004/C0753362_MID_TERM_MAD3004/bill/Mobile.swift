6b0774384743c4f31f5477ef5cd3eeeecc439d81
vipulgarg192 <vipulgarg192@gmail.com>
2019-06-20 18:50:17 -0400

    created mobile class basic

---
+++ C0753362_MID_TERM_MAD3004/C0753362_MID_TERM_MAD3004/bill/Mobile.swift
@@ -0,0 +1,31 @@
+//
+//  Mobile.swift
+//  C0753362_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-06-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Mobile: Bill {
+    var manufacturerName : String
+    var mobileNumber : Int
+    var internetUsed : String
+    var minutesUsed : String
+    
+    
+    init(billId: Int, billDate: Date, billType: String , manufacturerName : String ,
+                  mobileNumber : Int , internetUsed : String , minutesUsed : String) {
+        
+        self.manufacturerName = manufacturerName
+        self.mobileNumber = mobileNumber
+        self.internetUsed = internetUsed
+        self.minutesUsed = minutesUsed
+        super.init(billId: billId, billDate: billDate , billType : billType)
+    }
+    
+
+    
+}
+
