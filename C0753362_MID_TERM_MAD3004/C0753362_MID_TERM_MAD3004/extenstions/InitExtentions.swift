9cfdd21837868199df4f795d7a94a21e181ae8ed
vipulgarg192 <vipulgarg192@gmail.com>
2019-06-20 19:23:23 -0400

    Integer Extension created

---
+++ C0753362_MID_TERM_MAD3004/C0753362_MID_TERM_MAD3004/extenstions/InitExtentions.swift
@@ -0,0 +1,17 @@
+//
+//  InitExtentions.swift
+//  C0753362_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-06-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+extension Int{
+    
+    func UnitHyrdoConvert() ->String {
+       return "\(self) Units"
+    }
+    
+}
