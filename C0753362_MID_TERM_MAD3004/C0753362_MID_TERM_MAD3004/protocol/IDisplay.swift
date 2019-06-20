64a6384c5c2f9d7cc26ee4440a681cc9f2610c77
vipulgarg192 <vipulgarg192@gmail.com>
2019-06-20 19:11:00 -0400

    IDisplay protocol is created

---
+++ C0753362_MID_TERM_MAD3004/C0753362_MID_TERM_MAD3004/protocol/IDisplay.swift
@@ -0,0 +1,14 @@
+//
+//  IDisplay.swift
+//  C0753362_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-06-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+protocol IDisplay {
+    func Display()
+    
+}
