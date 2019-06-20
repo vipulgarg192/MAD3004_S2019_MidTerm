3d88c19cefeddeab12c5e31f22bdccdf873a00ac
vipulgarg192 <vipulgarg192@gmail.com>
2019-06-20 19:21:13 -0400

    float extension added

---
+++ C0753362_MID_TERM_MAD3004/C0753362_MID_TERM_MAD3004/extenstions/FloatExtention.swift
@@ -0,0 +1,21 @@
+//
+//  FloatExtention.swift
+//  C0753362_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-06-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+extension Float{
+    
+    func internetUnit() -> String {
+        return "\(self) gb"
+    }
+    
+    func minutesUnits() -> String {
+        return "\(self) minutes"
+    }
+    
+}
